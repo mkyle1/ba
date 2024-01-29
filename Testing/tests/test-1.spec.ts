41b070a8ed8fc3881271fab6fe3c668ddfc68370
Kyle Mezger <kylemezger@aol.de>
2024-01-29 16:02:53 +0100

    More text for chapter 3 and 4 + adding some sources for later use in chapter 4

--- Testing/tests/test-1.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://193.197.230.125/egroupware/login.php?cd=10&phpgw_forward=%252Findex.php');
-});
